827b776d246037c13583363558f7db8f48e3508c
James Forkey <Jmforkey@wpi.edu>
2012-07-30 20:06:28 -0400

    for real this time..

---
+++ VenueClassic/KeyboardExtensionAppDelegate.m
@@ -0,0 +1,34 @@
+//
+//
+//  initiated by Jason Lam (cofounder @WaveSpread) on 8/28/11.
+//  Copyright 2012 keyboards.hk
+//
+//  Initiators/root branches
+//      iOS code: https://github.com/jasonlamkk/iOSNumPadDoneButton/
+//      Concept and Graphic Reference : 2008/2009 Neoos GmbH
+//      http://www.neoos.ch/blog/37-uikeyboardtypenumberpad-and-the-missing-return-key
+//      Major Update :
+//        - support multiple textfields in same page, each of different type
+//        - support dynamic done button text by using empty button background image and textlable 
+//  Contributors:
+//      https://github.com/jasonlamkk/iOSNumPadDoneButton/contributors
+//
+#import "KeyboardExtensionAppDelegate.h"
+
+@implementation KeyboardExtensionAppDelegate
+
+@synthesize window;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+
+    [window makeKeyAndVisible];
+}
+
+
+- (void)dealloc {
+  
+}
+
+
+@end
